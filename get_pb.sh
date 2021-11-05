bda908b024fa989dfa272793e0c9b44e522fdb0f
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-11-05 14:07:42 -0600

    Update get_pb.sh

--- get_pb.sh
+++ get_pb.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/Ganesha2282882/paddleballjs.git node_modules/paddleballjs
 cd node_modules/paddleballjs
 mv index.js dev.js
-npx uglifyjs dev.js > index.js
+uglifyjs dev.js > index.js
 rm dev.js
 cd -
