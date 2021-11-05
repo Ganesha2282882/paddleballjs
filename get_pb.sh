9c1c5a052662f027f9e44c267f159b88b54261f3
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-11-05 14:02:37 -0600

    Create get_pb.sh

---
+++ get_pb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/Ganesha2282882/paddleballjs.git node_modules/paddleballjs
+cd node_modules/paddleballjs
+mv index.js dev.js
+uglifyjs dev.js > index.js
+rm dev.js
+cd -
